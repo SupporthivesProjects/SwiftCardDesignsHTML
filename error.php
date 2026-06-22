0e6a6452f15f91c38505d823c5580a042531d07f
vijay <vijay@supporthive.com>
2026-06-22 09:49:30 +0530

    header and footer updated

---
+++ error.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php'; ?>
+
+
+<section class="error-section">
+    <img src="./img/404-img.png" alt="" class="bg-image d-lg-block d-md-block d-none">
+    <img src="./img/404-img-mo.png" alt="" class="bg-image d-lg-none d-md-none d-block">
+    <div class="container">
+        <div class="col">
+            <div class="erron-content">
+                <h1>there seems to be an error</h1>
+                <p>
+                    Something went wrong, please try 
+                    again or return back to the homepage.
+                </p>
+                <div class="border-button">
+                    <a href="index.php" class="btn btn-green">return home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'includes/footer.php'; ?>
