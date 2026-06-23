cec2cb43b91e05348ab57f823bb5a390afda1644
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-06-23 06:47:05 +0530

    rahul

--- dashboard.php
+++ dashboard.php
@@ -135,6 +135,9 @@
                     <!-- ORDER HISTORY -->
                     <div class="tab-pane fade" id="orders">
                         <div class="oh_tab">
+                            <div class="desktop_none">
+                                <p class="table_swipetext"><img src="./img/swipedot.png">Swipe to view table</p>
+                            </div>
                             <div class="oh_tabinner">
                                 <div class="table-responsive order-table-wrapper">
                                     <table class="table mb-0 align-middle table-striped oder_tablemain">
@@ -153,7 +156,7 @@
                                                 <td>01.12.2023</td>
                                                 <td class="text-center">x3</td>
                                                 <td class="text-center">£199.75</td>
-                                                <td class="text-center"><a href="#">Order Details</a></td>
+                                                <td class="text-center"><a href="#" data-bs-toggle="modal" data-bs-target="#orderModal">Order Details</a></td>
                                             </tr>
                                             <tr>
                                                 <td>12345</td>
@@ -213,6 +216,142 @@
     </Section>
 <!-- Section 1 Start -->
 
+<!-- Order Modal -->
+ <div class="modal fade" id="orderModal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered order-modal">
+        <img src="./img/modal_topimg.svg">
+        <div class="modal-content order-modal-content">
+
+            <!-- Header -->
+            <div class="modal-header order_modalheader">
+                <h2 class="order_modalid">Order: #1234567</h2>
+                <div class="order_titleleft">
+                    <!-- Order Status -->
+                    <div class="d-flex justify-content-end order-status">
+                        <div class="status_card">
+                            <img src="./img/status1.png">
+                            <p>Order Placed</p>
+                        </div>
+
+                        <div class="status_card">
+                            <img src="./img/status2.png">
+                            <p>On Review</p>
+                        </div>
+
+                        <div class="status_card">
+                            <img src="./img/status3.png">
+                            <p>Pending</p>
+                        </div>
+
+                        <div class="status_card active">
+                            <img src="./img/status4.png">
+                            <p>Delivered</p>
+                        </div>
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+            </div>
+
+            <div class="modal-body">
+
+
+                <div class="row g-3">
+
+                    <!-- Purchase Overview -->
+                    <div class="col-lg-8">
+                        <div class="card order-card">
+                            <div class="card-header">
+                                Purchase Overview
+                            </div>
+
+                            <div class="card-body p-0">
+                                <table class="table mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Product</th>
+                                            <th>Type</th>
+                                            <th>Qty</th>
+                                            <th>Price</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr>
+                                            <td>1.</td>
+                                            <td>Learning Gutenberg</td>
+                                            <td>Video</td>
+                                            <td>1</td>
+                                            <td>$100.00</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>2.</td>
+                                            <td>Learning Gutenberg - Video</td>
+                                            <td>Video</td>
+                                            <td>2</td>
+                                            <td>$100.00</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>3.</td>
+                                            <td>Learning Gutenberg - Video</td>
+                                            <td>Video</td>
+                                            <td>3</td>
+                                            <td>$100.00</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Order Amount -->
+                    <div class="col-lg-4">
+                        <div class="card order-card">
+                            <div class="card-header">
+                                Order Amount
+                            </div>
+
+                            <div class="card-body">
+                                <!-- content -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Order Information -->
+                    <div class="col-lg-6">
+                        <div class="card order-card">
+                            <div class="card-header">
+                                Order Information
+                            </div>
+
+                            <div class="card-body">
+                                <!-- content -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Billing Information -->
+                    <div class="col-lg-6">
+                        <div class="card order-card">
+                            <div class="card-header">
+                                Billing Information
+                            </div>
+
+                            <div class="card-body">
+                                <!-- content -->
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
 <?php include 'includes/footer.php'; ?>
 <script>
     const toggleBtn = document.querySelector("#tabToggle");
